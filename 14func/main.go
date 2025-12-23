1ac0e1d3a65597b2e233038145c56a2c0547ea39
Pkra99 <pkra99bux@gmail.com>
2025-12-23 20:41:39 +0530

    creating server is ezz and fun

--- 14func/main.go
+++ 14func/main.go
@@ -1,5 +1,19 @@
 package main
 
+func add(a, b int) int {
+	return a + b
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+// aggregate is a higher order func
+func aggregate(a, b, c int, arithmetic func(int, int) int) int {
+	return arithmetic(arithmetic(a, b), c)
+}
+
 func main() {
-	
+	println("Add: ", aggregate(2, 4, 5, add))
+	println("Multiply: ", aggregate(2, 4, 5, multiply))
 }
